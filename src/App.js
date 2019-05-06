e6cd55bc5a8e60d5b7e676e7309ff07e9439a0fb
Arthur Attout <arthur.attout@outlook.com>
2019-05-07 01:25:46 +0200

    Base of interface

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Websocket from 'react-websocket';
 import './App.css';
+import { SortablePane, Pane } from 'react-sortable-pane';
+
+const titleStyle = {
+	color: "#000000",
+	fontSize: 28,
+	fontWeight: "bold",
+	backgroundColor: "#EDEDED",
+	marginBottom: 50,
+	padding: 25,
+	display: "inline-block",
+	fontFamily: "roboto"
+};
+
+const paneStyle = {
+	display: 'flex',
+	justifyContent: 'left',
+	padding: 30,
+	color: "#035218",
+	fontSize: 28,
+	backgroundColor: "#EDEDED",
+	fontFamily: "roboto"
+};
 
 class App extends Component {
-
+	
 	constructor(props) {
 		super(props);
 		this.state = { message: "nothing yet" };
@@ -22,30 +44,39 @@ class App extends Component {
 	}
 
 	render(){
-	 return (
-		<div className="App">
-		  <header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<p>
-			  Server says : {this.state.message}
-			</p>
-			<a
-			  className="App-link"
-			  href="#"
-			  target="_blank"
-			  onClick={this.handleClick.bind(this)}
-			  rel="noopener noreferrer">
-			  Learn React
-			</a>
-		  </header>
-		 <div>
-
-		 </div>
-		  <Websocket url='ws://192.168.1.85:8080/'
-              onMessage={this.handleData.bind(this)}
-			  ref={Websocket => {this.refWebSocket = Websocket;}} />
-		</div>
-	  );
+		const panes = []
+		panes[0] = (
+			<Pane key={0} defaultSize={{ width: '30%', height: 500 }} style={paneStyle}>
+			  Statistiques
+			</Pane>
+		)
+		
+		panes[1] = (
+			<Pane key={1} defaultSize={{ width: '30%', height: 500 }} style={paneStyle}>
+			  Dossiers de biomasses non identifiées
+			</Pane>
+		)
+		
+		panes[2] = (
+			<Pane key={2} defaultSize={{ width: '30%', height: 500 }} style={paneStyle}>
+			  Dernières identifications
+			</Pane>
+		)
+		
+		return (
+			<div style={{ padding: '10px' , backgroundColor: '#4286f4'}}>
+				<div style={titleStyle}>
+					BIOMASSE
+				</div>
+				<SortablePane direction="horizontal" style={{ height: 1000 }} margin={20} >
+					{panes}	
+				</SortablePane>
+			</div>
+			/*<Websocket url='ws://192.168.1.85:8080/'
+			onMessage={this.handleData.bind(this)}
+			ref={Websocket => {this.refWebSocket = Websocket;}} />*/
+			
+		);
 	}
 }
 
